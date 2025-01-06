b1c171f6856d1344d81de8d9259771b8ce077037
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-01-06 17:14:01 +0100

    Create vote entity

---
+++ src/Entity/Vote.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoteRepository::class)]
+class Vote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $votedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'votes')]
+    private ?User $voter = null;
+
+    #[ORM\ManyToOne(inversedBy: 'votes')]
+    private ?Election $election = null;
+
+    #[ORM\ManyToOne(inversedBy: 'votes')]
+    private ?Candidate $candidate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVotedAt(): ?\DateTimeImmutable
+    {
+        return $this->votedAt;
+    }
+
+    public function setVotedAt(\DateTimeImmutable $votedAt): static
+    {
+        $this->votedAt = $votedAt;
+
+        return $this;
+    }
+
+    public function getVoter(): ?User
+    {
+        return $this->voter;
+    }
+
+    public function setVoter(?User $voter): static
+    {
+        $this->voter = $voter;
+
+        return $this;
+    }
+
+    public function getElection(): ?Election
+    {
+        return $this->election;
+    }
+
+    public function setElection(?Election $election): static
+    {
+        $this->election = $election;
+
+        return $this;
+    }
+
+    public function getCandidate(): ?Candidate
+    {
+        return $this->candidate;
+    }
+
+    public function setCandidate(?Candidate $candidate): static
+    {
+        $this->candidate = $candidate;
+
+        return $this;
+    }
+}
