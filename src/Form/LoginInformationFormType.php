17481ae273b07affdba02e4ba273db201cb7578e
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-01-09 17:10:58 +0100

    first try for create user

--- src/Form/LoginInformationFormType.php
+++ src/Form/LoginInformationFormType.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +16,14 @@ class LoginInformationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
             ->add('company')
             ->add('jobTitle')
-        ;
+            ->add('avatar');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
