031cc11604b09b07899d29cdf89421eb1ad09805
d9beuD <d9beuD@users.noreply.github.com>
2025-01-10 15:33:28 +0100

    Clean up login controllers

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\LoginInformationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,8 +15,24 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(): Response
     {
-        return $this->render('connexion/index.html.twig', [
-            'controller_name' => 'LoginController',
+        return $this->render('login/index.html.twig');
+    }
+    
+    #[Route( name: 'app_login_complete', methods: ['GET', 'POST'])]
+    public function complete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(LoginInformationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_register', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('login_information/index.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
 }
