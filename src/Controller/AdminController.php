b83f6fa980d5d62d27f475dd951655c1eea19541
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-01-13 16:13:33 +0100

    Add controller

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    #[Route(name: 'app_admin_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+}
