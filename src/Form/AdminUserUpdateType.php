8cde30fa42ff4d5737c1ce9de22b0a48ebbc1d1a
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-01-13 16:13:49 +0100

    Create AdminUserUpdateType.php

---
+++ src/Form/AdminUserUpdateType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminUserUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            
+            ->add('email')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('username')
+            ->add('company')
+            ->add('jobTitle')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'User' => 'ROLE_USER',
+                ],
+                'expanded' => true,
+                'multiple' => true,
+            ])
+            ->add('avatar')
+            ->add('googleId')
+            ->add('microsoftId')
+            ->add('githubId')
+            // ->add('linkedinId')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
