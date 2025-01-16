1dbe1827bbec4240d462102e6d4a827b49c6438b
d9beuD <d9beuD@users.noreply.github.com>
2025-01-16 13:57:13 +0100

    Add findAllPaginated method on userRepository

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,31 +5,15 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @extends ServiceEntityRepository<User>
  */
-class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class UserRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
-    }
-
-    /**
-     * Used to upgrade (rehash) the user's password automatically over time.
-     */
-    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
-    {
-        if (!$user instanceof User) {
-            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
-        }
-
-        $this->getEntityManager()->persist($user);
-        $this->getEntityManager()->flush();
     }
 
     /**
@@ -44,39 +28,22 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
             ->getQuery()
             ->getResult();
     }
+    
+    /**
+    * @return User[] Returns an array of User objects
+    */
+    public function findAllPaginated(int $page = 1, int $perPage = 50, array $orderBy = []): array
+    {
+        $qb = $this->createQueryBuilder('u');
+        $qb
+            ->setMaxResults($perPage)
+            ->setFirstResult(($page - 1) * $perPage)
+        ;
 
-    // public function isRejected(int $id): bool
-    // {
-    //     $result = $this->createQueryBuilder('u')
-    //         ->andWhere('u.id = :id')
-    //         ->andWhere('u.rejectAt IS NOT NULL')
-    //         ->setParameter('id', $id)
-    //         ->getQuery()
-    //         ->getOneOrNullResult();
+        foreach ($orderBy as $sort => $order) {
+            $qb->orderBy($sort, $order);
+        }
 
-    //     return $result !== null;
-    // }
-
-    // public function isApproved(int $id): bool
-    // {
-    //     $result = $this->createQueryBuilder('u')
-    //         ->andWhere('u.id = :id')
-    //         ->andWhere('u.approvedAt IS NULL')
-    //         ->setParameter('id', $id)
-    //         ->getQuery()
-    //         ->getOneOrNullResult();
-
-    //     return $result !== null;
-    // }
-
-
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        return $qb->getQuery()->getResult();
+    }
 }
