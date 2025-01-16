45062383b3201f9629551bb623daad999e27fe68
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-01-16 16:16:39 +0100

    Redirection for home for not Approve
    As ROLE_USER and not ROLE_APPROVED you can't access to "/"

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(#[CurrentUser()] User $currentUser): Response
     {
+        if ($currentUser) {
+            $roles = $currentUser->getRoles();
+            if (in_array('ROLE_USER', $roles) && !in_array('ROLE_APPROVED', $roles)) {
+                return $this->redirectToRoute('app_complete_profile');
+            }
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
