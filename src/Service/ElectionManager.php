8e4ca3273fa2535cc159b3bece4119e26f3e8301
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-01-21 16:01:01 +0100

    Create vote method

--- src/Service/ElectionManager.php
+++ src/Service/ElectionManager.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use App\Entity\Candidate;
 use App\Entity\Election;
 use App\Entity\User;
+use App\Entity\Vote;
 
 final class ElectionManager
 {
@@ -13,6 +14,15 @@ final class ElectionManager
     {
         $candidate
             ->setCandidate($user)
+            ->setElection($election)
+        ;
+    }
+
+    public function vote(User $user, Vote $vote, Candidate $candidate, Election $election): void
+    {
+        $vote
+            ->setVoter($user)
+            ->setCandidate($candidate)
             ->setElection($election)
         ;
     }
