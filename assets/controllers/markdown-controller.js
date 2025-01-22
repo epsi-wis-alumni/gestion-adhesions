b1d545f4881b4f9c47d4f76b8ef7a75acb8d3b48
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-01-22 16:59:12 +0100

    test markdown editor

---
+++ assets/controllers/markdown-controller.js
@@ -0,0 +1,12 @@
+import { Controller } from '@hotwired/stimulus';
+import snarkdown from 'snarkdown';
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    static targets = ['input', 'preview'];
+
+    render() {
+        const rendered = snarkdown(this.inputTarget.value);
+        this.previewTarget.innerHTML = rendered;
+    }
+}
