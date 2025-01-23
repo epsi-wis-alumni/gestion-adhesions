9725ce3ac0ba9d97e596a5df42f3fe0b27e9a5dc
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-01-23 17:40:58 +0100

    create NewsletterForm for Newsletter form

--- src/Form/NewsletterType.php
+++ src/Form/NewsletterType.php
@@ -3,9 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Newsletter;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,22 +15,28 @@ class NewsletterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('objet')
-            ->add('body')
-            ->add('cta')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('objet', TextType::class, [
+                'attr' => ['class' => ''],
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Objet',
             ])
-            ->add('sendAt', null, [
-                'widget' => 'single_text',
+            ->add('body', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-widget p-3',
+                    'data-markdown-target' => 'input',
+                    'data-action' => 'input->markdown#render',
+                    'aria-label' => 'Type markdown into this box',
+                    'autocomplete' => 'off',
+                    'placeholder' => '# Corps de la Newsletter',
+                    'rows' => '8',
+                ],
+                'row_attr' => ['class' => 'mb-0'],
+                'label' => False,
             ])
-            ->add('createdBy', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
-            ->add('sentBy', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('cta', TextType::class, [
+                'attr' => ['class' => ''],
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Call To Action',
             ])
         ;
     }
