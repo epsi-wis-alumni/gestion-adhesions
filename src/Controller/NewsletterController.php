77f6852f2e25bb48133f155e1684a6dd808a86c4
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-01-27 14:33:02 +0100

    Create NewsletterController.php

---
+++ src/Controller/NewsletterController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Newsletter;
+use App\Repository\NewsletterRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/newsletter')]
+class NewsletterController extends AbstractController
+{
+    #[Route('/{id}', name: 'app_newsletter_show')]
+    public function index(
+        Newsletter $newsletter,
+        NewsletterRepository $newsletterRepository,
+    ): Response {
+
+        $currentNewsletter = $newsletterRepository->find($newsletter);
+        $templateFileName = $currentNewsletter->getTemplate()->getFileName();
+
+        if (!file_exists(realpath(__DIR__ . '/../../templates/mails/' . $templateFileName . '.html.twig'))) {
+            $templateFileName = 'default_template';
+        }
+        
+        return $this->render('mails/' . $templateFileName . '.html.twig', [
+            'currentNewsletter' => $currentNewsletter,
+        ]);
+    }
+}
