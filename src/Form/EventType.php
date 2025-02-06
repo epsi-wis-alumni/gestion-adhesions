d63346fb8900f06a20eec080209b10277ce01f38
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-02-06 12:23:46 +0100

    Create EventType.php

---
+++ src/Form/EventType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ex : Soirée d\'ouverture',
+                ],
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Titre',
+            ])
+            ->add('place', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Ex : 349 rue de la Cavalade Montpellier 34000',
+                ],
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Lieu',
+            ])
+            ->add('startAt', null, [
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Date de début',
+            ])
+            ->add('endAt', null, [
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Date de fin',
+            ])
+            ->add('private', CheckboxType::class, [
+                'label' => 'Privé',
+                'label_attr' => [
+                    'class' => 'checkbox-switch',
+                ],
+                'row_attr' => ['class' => 'mb-3'],
+                'required' => false,
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
