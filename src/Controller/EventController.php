2a109a43c701286979a1b040d70d3212d6397b5e
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-02-07 11:27:35 +0100

    Add public and private distinction

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Event;
 use App\Repository\EventRepository;
+use App\Service\EventManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,17 +13,24 @@ use Symfony\Component\Routing\Attribute\Route;
 final class EventController extends AbstractController
 {
     #[Route(name: 'app_event_index', methods: ['GET'])]
-    public function index(EventRepository $eventRepository): Response
-    {
+    public function index(
+        EventRepository $eventRepository,
+        EventManager $eventManager,
+    ): Response {
         $pendingEvents = $eventRepository->findPending();
+        $publicPendingEvents = $eventManager->getPublic($pendingEvents);
         $inProgressEvents = $eventRepository->findInProgress();
+        $publicInProgressEvents = $eventManager->getPublic($inProgressEvents);
         $doneEvents = $eventRepository->findDone();
-
+        $publicDoneEvents = $eventManager->getPublic($doneEvents);
+        
         return $this->render('event/index.html.twig', [
-            'isClose' => false,
             'pending_events' => $pendingEvents,
+            'public_pending_events' => $publicPendingEvents,
             'in_progress_events' => $inProgressEvents,
+            'public_in_progress_events' => $publicInProgressEvents,
             'done_events' => $doneEvents,
+            'public_done_events' => $publicDoneEvents,
         ]);
     }
 
