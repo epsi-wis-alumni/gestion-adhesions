cc13616412b636fbf78905099e8ebf59c13f017a
d9beuD <d9beuD@users.noreply.github.com>
2025-02-10 15:20:02 +0100

    Update EventManager.php

--- src/Service/EventManager.php
+++ src/Service/EventManager.php
@@ -4,15 +4,10 @@ namespace App\Service;
 
 use App\Entity\Event;
 use App\Entity\User;
-use App\Repository\EventRepository;
 use DateTimeImmutable;
 
 final class EventManager
 {
-    public function __construct(
-        private EventRepository $eventRepository,
-    ) {}
-
     public function create(User $user, Event $event): void
     {
         $event
@@ -28,13 +23,4 @@ final class EventManager
             ->setUpdatedAt(new DateTimeImmutable())
         ;
     }
-
-    public function getPublic(array $events): array
-{
-    $publicEvents = $this->eventRepository->findByPublic();
-
-    return array_filter($events, function ($event) use ($publicEvents) {
-        return in_array($event, $publicEvents, true);
-    });
-}
 }
