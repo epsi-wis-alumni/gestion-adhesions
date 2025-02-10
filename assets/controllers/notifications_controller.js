5737147c5d0b2c5de372828ca14f49e42919f73c
d9beuD <d9beuD@users.noreply.github.com>
2025-02-10 17:24:11 +0100

    Fix stimulus controller

--- assets/controllers/notifications_controller.js
+++ assets/controllers/notifications_controller.js
@@ -1,19 +1,25 @@
 import { Controller } from '@hotwired/stimulus';
+import { Collapse } from 'bootstrap';
 
 export default class extends Controller {
-  static targets = ["notifications", "notificationList", "notification"];
-
+  static targets = ["notifications", 'notificationDetails'];
+  
   connect() {
+    this.notificationCollapse = new Collapse(
+      this.notificationDetailsTarget,
+      { toggle: this.notificationsTarget.checked}
+    );
     this.showNotification();
   }
 
   showNotification() {
-    this.notificationListTarget.style.display = this.notificationsTarget.checked ? 'block' : 'none';
+    this.notificationsTarget.checked ? this.notificationCollapse.show() : this.notificationCollapse.hide();
   }  
 
   syncNotification() {
-    if (this.hasNotificationTarget) {
-      this.notificationTarget.checked = this.notificationsTarget.checked;
+    if (!this.notificationsTarget.checked) {
+      const checkboxes = this.notificationDetailsTarget.querySelectorAll('input[type="checkbox"]');
+      checkboxes.forEach(checkbox => checkbox.checked = this.notificationsTarget.checked);
     }
   }
 
