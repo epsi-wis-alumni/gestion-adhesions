0de392487beb2a1aec1877f9471c27e8cc47e5c8
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-02-11 16:22:02 +0100

    Use text type for presentation

--- src/Entity/Candidacy.php
+++ src/Entity/Candidacy.php
@@ -6,6 +6,7 @@ use App\Repository\CandidacyRepository;
 use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CandidacyRepository::class)]
@@ -32,7 +33,7 @@ class Candidacy
     #[ORM\OneToMany(targetEntity: Vote::class, mappedBy: 'candidacy')]
     private Collection $votes;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $presentation = null;
 
     public function __construct()
