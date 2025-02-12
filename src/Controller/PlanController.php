0298fe29cc2b059b18135c158e2a72767721e095
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-12 12:01:56 +0100

    move all methods (except show)

--- src/Controller/PlanController.php
+++ src/Controller/PlanController.php
@@ -22,60 +22,11 @@ final class PlanController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_plan_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $plan = new Plan();
-        $form = $this->createForm(PlanType::class, $plan);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($plan);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_plan_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('plan/new.html.twig', [
-            'plan' => $plan,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/{id}', name: 'app_plan_show', methods: ['GET'])]
     public function show(Plan $plan): Response
     {
         return $this->render('plan/show.html.twig', [
             'plan' => $plan,
         ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_plan_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Plan $plan, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(PlanType::class, $plan);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_plan_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('plan/edit.html.twig', [
-            'plan' => $plan,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_plan_delete', methods: ['POST'])]
-    public function delete(Request $request, Plan $plan, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$plan->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($plan);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_plan_index', [], Response::HTTP_SEE_OTHER);
     }
 }
