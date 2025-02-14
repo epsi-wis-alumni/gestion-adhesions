9b087e6538cbbfb39c26b491e3fa18ee89d81e20
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-14 10:57:57 +0100

    add mailing for invoice

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Subscription;
 use App\Entity\User;
+use App\Repository\SubscriptionRepository;
+use App\Service\InvoiceManager;
+use App\Service\TransactionManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,6 +21,35 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('app_complete_profile', [], Response::HTTP_SEE_OTHER);
         }
 
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/test', name: 'app_home_test')]
+    public function test(
+        TransactionManager $transactionManager,
+        InvoiceManager $invoiceManager,
+        #[CurrentUser()] User $user,
+        SubscriptionRepository $subscriptionRepository,
+    ): Response {
+        $subscription = $subscriptionRepository->findOneBy(["id" => 1]);
+        $transaction = $transactionManager->create(
+            user: $user,
+            subscription: $subscription,
+            status: 1,
+            type: 2,
+            amount: 5.00
+        );
+
+        $html = $this->render('invoice/invoice.html.twig', [
+            'transaction' => $transaction,
+            'user' => $user,
+        ]);
+        $invoiceManager->create(
+            html: $html,
+            transaction: $transaction
+        );
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
