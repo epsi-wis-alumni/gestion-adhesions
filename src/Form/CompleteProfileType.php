05787532a4f3b4a86d49ccf5001d7d4e0a49ff43
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-14 14:36:48 +0100

    merge EditProfileType and CompleteProfileType

--- src/Form/CompleteProfileType.php
+++ src/Form/CompleteProfileType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,51 +11,39 @@ class CompleteProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('email', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'placeholder' => 'Ex : jeanmarc@contact.com',
-                ],
-                'row_attr' => ['class' => 'mb-3'],
-            ])
-            ->add('firstname', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'placeholder' => 'Ex : Jean-Marc',
-                ],
-                'row_attr' => ['class' => 'mb-3'],
-                'label' => 'Prénom'
-            ])
-            ->add('lastname', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'placeholder' => 'Ex : Dupont',
-                ],
-                'row_attr' => ['class' => 'mb-3'],
-                'label' => 'Nom',
-            ])
-            ->add('company', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'placeholder' => 'Ex : EPSI-WIS Alumni',
-                ],
-                'row_attr' => ['class' => 'mb-3'],
-                'label' => 'Entreprise',
-            ])
-            ->add('jobTitle', null, [
-                'attr' => [
-                    'class' => 'form-control',
-                    'placeholder' => 'Ex : Data Scientist',
-                ],
-                'row_attr' => ['class' => 'mb-3'],
-                'label' => 'Poste',
-            ])
-            ->add('save', SubmitType::class, [
-                'attr' => ['class' => 'btn btn-primary'],
-                'row_attr' => ['class' => 'mb-3'],
-                'label' => 'Enregistrer',
-            ]);
+        $builder->add('firstname', null, [
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => 'Ex : Jean-Marc',
+                    ],
+                    'row_attr' => ['class' => 'mb-3'],
+                    'label' => 'Prénom'
+                ])
+                ->add('lastname', null, [
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => 'Ex : Dupont',
+                    ],
+                    'row_attr' => ['class' => 'mb-3'],
+                    'label' => 'Nom',
+                ])
+                ->add('company', null, [
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => 'Ex : EPSI-WIS Alumni',
+                    ],
+                    'row_attr' => ['class' => 'mb-3'],
+                    'label' => 'Entreprise',
+                ])
+                ->add('jobTitle', null, [
+                    'attr' => [
+                        'class' => 'form-control',
+                        'placeholder' => 'Ex : Data Scientist',
+                    ],
+                    'row_attr' => ['class' => 'mb-3'],
+                    'label' => 'Poste',
+                ])
+                ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
