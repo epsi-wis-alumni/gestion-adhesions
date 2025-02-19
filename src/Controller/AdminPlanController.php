d327d18d20e9fc29b565a5a836157ca117e1c2e5
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-19 13:27:24 +0100

    Update AdminPlanController.php

--- src/Controller/AdminPlanController.php
+++ src/Controller/AdminPlanController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Plan;
 use App\Form\AdminPlanType;
 use App\Repository\PlanRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,25 +24,22 @@ final class AdminPlanController extends AbstractController
     }
 
     #[Route('/new', name: 'app_admin_plan_new', methods: ['GET', 'POST'])]
-    public function new(Request $request,
-    EntityManagerInterface $entityManager,
-    PlanRepository $planRepository,
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager
     ): Response {
         $plan = new Plan();
         $form = $this->createForm(AdminPlanType::class, $plan);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $planRepository->resetAllHighlighted();
-
             $entityManager->persist($plan);
             $entityManager->flush();
-
             return $this->redirectToRoute('app_admin_plan_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('admin/plan/new.html.twig', [
-            'plan' => $plan,
             'form' => $form,
         ]);
     }
@@ -56,26 +54,35 @@ final class AdminPlanController extends AbstractController
 
     #[Route('/{id}/edit', name: 'app_admin_plan_edit', methods: ['GET', 'POST'])]
     public function edit(
-        Request $request,
-        Plan $plan,
-        EntityManagerInterface $entityManager,
-        PlanRepository $planRepository,
+        Plan $plan, 
+        Request $request, 
+        EntityManagerInterface $entityManager
     ): Response {
+        $originalFeature = new ArrayCollection();
+
+        foreach ($plan->getFeatures() as $feature) {
+            $originalFeature->add($feature);
+        }
+
         $form = $this->createForm(AdminPlanType::class, $plan);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            if ($plan->isHighlighted()) {
-                $planRepository->resetAllHighlighted();
+            foreach ($originalFeature as $feature) {
+                if (false === $plan->getFeatures()->contains($feature)) {
+                    $feature->getPlan()->removeElement($feature);
+                    $entityManager->persist($feature);
+                    $entityManager->remove($feature);
+                }
             }
 
+            $entityManager->persist($plan);
             $entityManager->flush();
-
             return $this->redirectToRoute('app_admin_plan_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('admin/plan/edit.html.twig', [
-            'plan' => $plan,
+        return $this->render('admin/plan/new.html.twig', [
             'form' => $form,
         ]);
     }
@@ -83,11 +90,17 @@ final class AdminPlanController extends AbstractController
     #[Route('/{id}', name: 'app_admin_plan_delete', methods: ['POST'])]
     public function delete(Request $request, Plan $plan, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$plan->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$plan->getId(), $request->get('_token'))) {
+            foreach ($plan->getFeatures() as $feature) {
+                $feature->getPlan()->removeElement($feature);
+                $entityManager->remove($feature);
+            }
+
             $entityManager->remove($plan);
             $entityManager->flush();
         }
 
         return $this->redirectToRoute('app_admin_plan_index', [], Response::HTTP_SEE_OTHER);
     }
+
 }
