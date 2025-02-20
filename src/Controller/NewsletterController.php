9340c78162585de07d632c981c03a4d44bac05b7
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-02-20 16:20:06 +0100

    Add Finder to NewsletterController

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\UserNewsletter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,12 +16,15 @@ class NewsletterController extends AbstractController
     public function index(
         UserNewsletter $userNewsletter,
         ContainerBagInterface $params,
-        ): Response {
-
+    ): Response {
         $templateFileName = $userNewsletter->getNewsletter()->getTemplate()->getFileName();
+        $basePath = realpath(__DIR__ . $params->get('mails_base_path'));
 
-        if (!file_exists(realpath(__DIR__ . $params->get('mails_base_path') . $templateFileName))) {
-            $userNewsletter = 'userNewsletter';
+        $finder = new Finder();
+        $finder->files()->in($basePath)->name($templateFileName);
+
+        if (!$finder->hasResults()) {
+            throw $this->createNotFoundException('Le modèle de mail demandé est introuvable.');
         }
 
         return $this->render('mails/' . $templateFileName, [
