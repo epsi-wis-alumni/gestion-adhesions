87f7bf783f198f948b1fbc7ca17cb121b596a1fd
LenderDiam <151174455+LenderDiam@users.noreply.github.com>
2025-02-25 15:55:31 +0100

    Add monaco editor to markdown stimulus

--- assets/controllers/markdown-controller.js
+++ assets/controllers/markdown-controller.js
@@ -1,17 +1,52 @@
 import { Controller } from '@hotwired/stimulus';
 import markdownit from 'markdown-it'
+import { editor } from 'monaco-editor';
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-    static targets = ['input', 'preview'];
+    static targets = ['input', 'preview', 'editorContainer', 'editor'];
 
     connect() {
-        this.render();
+        const model = editor.createModel(this.inputTarget.value, 'markdown');
+        
+        this.editorInstance = editor.create(this.editorTarget, {
+            model,
+            theme: 'vs-dark',
+            automaticLayout: true,
+            scrollBeyondLastLine: false,
+            onDidContentSizeChange: () => {
+                this.adaptLayout();
+            },
+        });
+
+        
+        model.onDidChangeContent(() => {
+            const value = model.getValue();
+            this.inputTarget.value = this.editorInstance.getValue();
+            this.render();
+        });
+
+        this.editorInstance.onDidContentSizeChange(() => {
+            this.adaptLayout();
+        });
+
+        this.adaptLayout();
+    }
+
+    adaptLayout() {
+        const height = this.editorInstance.getContentHeight() < 200 ? 200 : this.editorInstance.getContentHeight();
+        this.editorContainerTarget.style.height = `${height}px`;
     }
     
     render() {
         const md = markdownit();
         const rendered = md.render(this.inputTarget.value);
         this.previewTarget.innerHTML = rendered;
+    }
+
+    disconnect() {
+        if (this.editorInstance) {
+            this.editorInstance.dispose();
+        }
     }
 }
