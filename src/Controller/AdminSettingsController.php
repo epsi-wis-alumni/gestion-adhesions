57ea503a9039836386d9a4820f166b7b6caf8844
d9beuD <d9beuD@users.noreply.github.com>
2025-03-02 21:30:54 +0100

    Add possibility to create a new mail template

--- src/Controller/AdminSettingsController.php
+++ src/Controller/AdminSettingsController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\MailTemplate;
+use App\Form\MailTemplateType;
 use App\Repository\MailTemplateRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -26,5 +30,27 @@ final class AdminSettingsController extends AbstractController
         $this->addFlash('success', 'Le cache a bien été vidé.');
 
         return $this->redirectToRoute('app_admin_settings');
+    }
+
+    #[Route('/mail-template/new', name: 'app_admin_settings_mailtemplate_new', methods: ['GET', 'POST'])]
+    public function newMailTemplate(Request $request, EntityManagerInterface $entityManager)
+    {
+        $mailTemplate = new MailTemplate();
+        $form = $this->createForm(MailTemplateType::class, $mailTemplate);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($mailTemplate);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le modèle a bien été créé.');
+
+            return $this->redirectToRoute('app_admin_settings', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('/admin/mail-template/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
