5e25edcc45512d4cee11022f7d573098d0abfbcc
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-03-05 17:25:50 +0100

    add form into contact page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use App\Service\ContactManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
-    {
+    public function index(
+        Request $request,
+        ContactManager $contactManager,
+    ): Response {
+        $form = $this->createForm(ContactType::class, null);
+        $form->handleRequest($request);
+            $contactManager->sendMail($form->getData());
+
+            return $this->redirectToRoute('app_contact', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('contact/index.html.twig', [
+            'form' => $form,
             'controller_name' => 'ContactController',
         ]);
     }
