19e6f9a73e9f49b3db79870e405d4e4e624eccd4
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-03-11 11:27:55 +0100

    Add findOneActivePlanTransactionByUser in TransactionRepository

--- src/Repository/TransactionRepository.php
+++ src/Repository/TransactionRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\Transaction;
+use App\Entity\User;
+use App\Enum\TransactionStatus;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +16,22 @@ class TransactionRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Transaction::class);
+    }
+
+    public function findOneActivePlanTransactionByUser(User $user): ?Transaction
+    {
+        return $this->createQueryBuilder('t')
+            ->leftJoin('t.user', 'u')
+            ->where('t.user = :user')
+            ->andWhere('t.createdAt >= :date')
+            ->andWhere('t.status = :status')
+            ->orderBy('t.createdAt', 'DESC')
+            ->setParameter('user', $user)
+            ->setParameter('date', new \DateTimeImmutable('-1 year'))
+            ->setParameter('status', TransactionStatus::Completed)
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
     }
 }
