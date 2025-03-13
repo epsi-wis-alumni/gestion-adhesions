8d943fc9c9fcb1951648586745959311f9fd8754
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-03-13 15:54:43 +0100

    Create DonnationController.php

---
+++ src/Controller/DonnationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Donation;
+use App\Form\DonationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/donation')]
+final class DonnationController extends AbstractController
+{
+    #[Route('/new', name: 'app_donnation', methods: ['GET', 'POST'])]
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+    ): Response
+    {
+        $donation = new Donation();
+        $form = $this->createForm(DonationType::class, $donation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $donation->setCreatedAt();
+            $entityManager->persist($donation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_payment_donation', ["id" => $donation->getId()], Response::HTTP_SEE_OTHER);
+    }
+
+        return $this->render('donnation/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
