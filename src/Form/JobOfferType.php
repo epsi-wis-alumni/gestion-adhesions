d08b7275a70a915717796aa11ca2f096319a974d
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-03-27 17:44:52 +0100

    Create JobOfferType.php

---
+++ src/Form/JobOfferType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\JobOffer;
+use App\Entity\Skill;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JobOfferType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('type')
+            ->add('status')
+            ->add('startAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('endAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('imageFilePath')
+            ->add('skills', EntityType::class, [
+                'class' => Skill::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('categories', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('createdBy', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => JobOffer::class,
+        ]);
+    }
+}
