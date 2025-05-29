d6cf60381ccd3763eeea98fe495a18abdaf7486f
Enzo Palermo <palermo.enzo.ep@gmail.com>
2025-05-29 18:12:43 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,7 @@ use App\Repository\EventRepository;
 use App\Repository\PlanRepository;
 use App\Repository\UserRepository;
 use App\Service\Manager;
+use App\Service\NotificationManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -23,6 +24,7 @@ class HomeController extends AbstractController
         EventRepository $eventRepository,
         UserRepository $userRepository,
         Manager $manager,
+        NotificationManager $notificationManager,
     ): Response {
         if ($currentUser && !$currentUser->hasCompleteInfo()) {
             if (!$currentUser->isAccountCreationEmailSent()) {
